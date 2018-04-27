16cc9c968701d2ba9c36d0d550fdf12458aa35e6
Archana Bansal <archanamittal0388@gmail.com>
2018-04-27 22:46:32 +0530

    Feat: Refactor the code

--- scripts/index.js
+++ scripts/index.js
@@ -1,53 +1,53 @@
 var config = {
-	BASE_URL: 'https://fcc-weather-api.glitch.me/',
-	WEATHER_API: 'api/current',
-	SEARCH_URL: 'https://maps.googleapis.com/maps/api/geocode/json',
-};
+  BASE_URL: 'https://fcc-weather-api.glitch.me/',
+  WEATHER_API: 'api/current',
+  SEARCH_URL: 'https://maps.googleapis.com/maps/api/geocode/json'
+}
 
-var Utility = (function() {
-	function makeAjaxRequest(url) {
-		return $.ajax({
-			url: url,
-		});
-	}
-	return {
-		ajaxRequest: makeAjaxRequest,
-	};
+var Utility = (function () {
+  function makeAjaxRequest (url) {
+    return $.ajax({
+      url: url
+    })
+  }
+  return {
+    ajaxRequest: makeAjaxRequest
+  }
 })();
 
-(function() {
-	var WeatherApp = function(location, search) {
-		this.location = location;
-		this.search = search;
-		this.init();
-	};
+(function () {
+  var WeatherApp = function (location, search) {
+    this.location = location
+    this.search = search
+    this.init()
+  }
 
-	WeatherApp.prototype.init = function() {
-		this.attachEvent();
-	};
+  WeatherApp.prototype.init = function () {
+    this.attachEvent()
+  }
 
-	WeatherApp.prototype.onCurrentPositionResponse = function(position) {
-		var URL = `${config.BASE_URL}${config.WEATHER_API}?lat=${position.coords
-			.latitude}&lon=${position.coords.longitude}`;
+  WeatherApp.prototype.onCurrentPositionResponse = function (position) {
+    var URL = `${config.BASE_URL}${config.WEATHER_API}?lat=${position.coords
+      .latitude}&lon=${position.coords.longitude}`
 
-		this.makeAjaxRequest(URL);
-	};
+    this.makeAjaxRequest(URL)
+  }
 
-	WeatherApp.prototype.makeAjaxRequest = function(URL) {
-		Utility.ajaxRequest(URL)
-			.done(this.handleResponse.bind(this))
-			.fail(function(err) {
-				console.log('Error: ' + err.status);
-			});
-	};
+  WeatherApp.prototype.makeAjaxRequest = function (URL) {
+    Utility.ajaxRequest(URL)
+      .done(this.handleResponse.bind(this))
+      .fail(function (err) {
+        console.log('Error: ' + err.status)
+      })
+  }
 
-	WeatherApp.prototype.handleResponse = function(data) {
-		this.responseData = data;
+  WeatherApp.prototype.handleResponse = function (data) {
+    this.responseData = data
 
-		this.faren = Math.round(data.main.temp * 9 / 5 + 32);
-		this.celcius = data.main.temp;
+    this.faren = Math.round(data.main.temp * 9 / 5 + 32)
+    this.celcius = data.main.temp
 
-		var temp = `<div class = 'current-location'>\
+    var temp = `<div class = 'current-location'>\
 									<strong class='location-head'>Location: </strong> 
 									<span class='location-name'>${data.name}</span>\
 								</div>\
@@ -64,61 +64,61 @@ var Utility = (function() {
 								</div>\
 								<div class = 'image'>\
 									<img src=" ${data.weather[0].icon}"></img>\
-								</div>`;
+								</div>`
 
-		this.location.html(temp);
-	};
+    this.location.html(temp)
+  }
 
-	WeatherApp.prototype.attachEvent = function() {
-		this.location.on('click', '.degree', this.handleClick.bind(this));
-		this.search.on('click', '.search-button', this.handleSearchClick.bind(this));
-		this.search.on('click', '.auto-button', this.handleCurrentLocation.bind(this));
-		this.search.on('keypress', '.pincode-text', this.handleKeyPress.bind(this));
-	};
+  WeatherApp.prototype.attachEvent = function () {
+    this.location.on('click', '.degree', this.handleClick.bind(this))
+    this.search.on('click', '.search-button', this.handleSearchClick.bind(this))
+    this.search.on('click', '.auto-button', this.handleCurrentLocation.bind(this))
+    this.search.on('keypress', '.pincode-text', this.handleKeyPress.bind(this))
+  }
 
-	WeatherApp.prototype.handleCurrentLocation = function() {
-		navigator.geolocation.getCurrentPosition(this.onCurrentPositionResponse.bind(this));
-		this.search.find('.pincode-text').val('');
-	};
+  WeatherApp.prototype.handleCurrentLocation = function () {
+    navigator.geolocation.getCurrentPosition(this.onCurrentPositionResponse.bind(this))
+    this.search.find('.pincode-text').val('')
+  }
 
-	WeatherApp.prototype.handleClick = function() {
-		this.location.find('.temp-celcius').toggleClass('hide');
-		this.location.find('.temp-farenheit').toggleClass('hide');
-	};
+  WeatherApp.prototype.handleClick = function () {
+    this.location.find('.temp-celcius').toggleClass('hide')
+    this.location.find('.temp-farenheit').toggleClass('hide')
+  }
 
-	WeatherApp.prototype.handleKeyPress = function(event) {
-		if (event.keyCode === 13) {
-			this.handleSearchClick();
-		}
-	};
+  WeatherApp.prototype.handleKeyPress = function (event) {
+    if (event.keyCode === 13) {
+      this.handleSearchClick()
+    }
+  }
 
-	WeatherApp.prototype.handleSearchClick = function() {
-		const pincodeValue = this.search.find('.pincode-text').val();
-		const URL = `${config.SEARCH_URL}?address=${pincodeValue}`;
-		this.searchAjaxRequest(URL);
-	};
+  WeatherApp.prototype.handleSearchClick = function () {
+    const pincodeValue = this.search.find('.pincode-text').val()
+    const URL = `${config.SEARCH_URL}?address=${pincodeValue}`
+    this.searchAjaxRequest(URL)
+  }
 
-	WeatherApp.prototype.searchAjaxRequest = function(URL) {
-		Utility.ajaxRequest(URL)
-			.done(response => {
-				if (response.results.length > 0) {
-					const latitude = response.results[0].geometry.location.lat;
-					const longitude = response.results[0].geometry.location.lng;
-					var weatherUrl = `${config.BASE_URL}${config.WEATHER_API}?lat=${latitude}&lon=${longitude}`;
-					this.makeAjaxRequest(weatherUrl);
-				} else {
-					var errorMsg = `<div class='error-msg'>No match found.Please enter valid pincode</div>`;
-					this.location.html(errorMsg);
-				}
-			})
-			.fail(function(err) {
-				console.log('Error: ' + err.status);
-			});
-	};
+  WeatherApp.prototype.searchAjaxRequest = function (URL) {
+    Utility.ajaxRequest(URL)
+      .done(response => {
+        if (response.results.length > 0) {
+          const latitude = response.results[0].geometry.location.lat
+          const longitude = response.results[0].geometry.location.lng
+          var weatherUrl = `${config.BASE_URL}${config.WEATHER_API}?lat=${latitude}&lon=${longitude}`
+          this.makeAjaxRequest(weatherUrl)
+        } else {
+          var errorMsg = `<div class='error-msg'>No match found.Please enter valid pincode</div>`
+          this.location.html(errorMsg)
+        }
+      })
+      .fail(function (err) {
+        console.log('Error: ' + err.status)
+      })
+  }
 
-	window.WeatherApp = WeatherApp;
+  window.WeatherApp = WeatherApp
 })();
 
-(function() {
-	new WeatherApp($('#currLocation'), $('.search-container'));
-})();
+(function () {
+  new WeatherApp($('#currLocation'), $('.search-container'))
+})()
